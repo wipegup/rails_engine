fcff6584cd706f77c5f2d28d17b5b2039218ba55
wipegup <wipegup@gmail.com>
2019-04-30 12:09:07 -0600

    outline for method tests in merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
+  describe 'class methods' do
+    it '.most_revenue(limit)' do
+
+    end
+
+    it '.most_items(limit)' do
+
+    end
+
+    it '.revenue(date)' do
+
+    end
+  end
+
+  describe 'instance methods' do
+    it '.revenue' do
+
+    end
+
+    it '.revenue(date)' do
+
+    end
+
+    it '.favorite_customer' do
+
+    end
+  end
   pending "add some examples to (or delete) #{__FILE__}"
 end
