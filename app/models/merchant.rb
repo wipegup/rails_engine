d0b3fd2ec34b95503f30b8171d665668dd5ad63e
wipegup <wipegup@gmail.com>
2019-04-30 13:13:53 -0600

    Add connection between merchant and transactions

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,8 +2,9 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
   has_many :items
   has_many :invoices
-
+  has_many :transactions, through: :invoices
+  
   def self.most_revenue(limit = nil)
-    # binding.pry
+    binding.pry
   end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -65,5 +65,6 @@ RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it {should have_many :items}
     it {should have_many :invoices}
+    it {should have_many(:transactions).through :invoices}
   end
 end
