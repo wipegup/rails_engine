c0b63a2794842dab8c1fc6de072d6583738fbbd2
wipegup <wipegup@gmail.com>
2019-04-30 10:33:51 -0600

    Passing simple items test

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :merchants, only: [:index]
       resources :customers, only: [:index]
+      resources :items, only: [:index]
     end
   end
 end
