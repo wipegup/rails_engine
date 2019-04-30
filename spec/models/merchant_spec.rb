a64b7a6f346b6d8fb65cc4996f4b466d053c9a81
wipegup <wipegup@gmail.com>
2019-04-30 14:19:38 -0600

    Add test for most items

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,47 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
+  before :each do
+    @merchants = []; @invoices = []; @items = []
+    5.times do
+      merchant = create(:merchant)
+      invoice = create(:invoice, merchant: merchant)
+      item = create(:item, merchant: merchant)
+      @merchants << merchant; @invoices << invoice; @items << item
+    end
+
+    @invoices.each_with_index do |invoice, idx|
+      create(:invoice_item, unit_price: idx, quantity: idx, invoice: invoice, item: @items[idx])
+      create(:transaction, result: 'success', invoice: invoice)
+
+      if idx % 3 == 0
+        create(:transaction, result: 'failed', invoice: invoice)
+      end
+    end
+  end
   describe 'class methods' do
     it '.most_revenue(limit)' do
 
-      merchants = []; invoices = []; items = []
-      5.times do
-        merchant = create(:merchant)
-        invoice = create(:invoice, merchant: merchant)
-        item = create(:item, merchant: merchant)
-        merchants << merchant; invoices << invoice; items << item
-      end
-      # binding.pry
-      invoices.each_with_index do |invoice, idx|
-        create(:invoice_item, unit_price: idx, quantity: idx, invoice: invoice, item: items[idx])
-        create(:transaction, result: 'success', invoice: invoice)
-
-        if idx % 3 == 0
-          create(:transaction, result: 'failed', invoice: invoice)
-
-
-        end
-
-        # binding.pry
-      end
-
-      expected = merchants[3..4].reverse
+      expected = @merchants[3..4].reverse
       actual = Merchant.most_revenue(2)
 
       expect(actual).to eq(expected)
 
-      expected = merchants[2..4].reverse
+      expected = @merchants[2..4].reverse
       actual = Merchant.most_revenue(3)
 
       expect(actual).to eq(expected)
 
-      expected = merchants.reverse
+      expected = @merchants.reverse
       actual = Merchant.most_revenue
 
       expect(actual).to eq(expected)
     end
 
     it '.most_items(limit)' do
+
+      expected = @merchants[3..4].reverse
+      actual = Merchant.most_items(2)
+
+      expect(actual).to eq(expected)
+
+      expected = @merchants[2..4].reverse
+      actual = Merchant.most_items(3)
+
+      expect(actual).to eq(expected)
+
+      expected = @merchants.reverse
+      actual = Merchant.most_items
+
+      expect(actual).to eq(expected)
 
     end
 
