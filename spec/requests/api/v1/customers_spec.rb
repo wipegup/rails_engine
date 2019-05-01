e8f080259bc14b7e6c486b7a11ab5dad44dcd606
wipegup <wipegup@gmail.com>
2019-05-01 14:07:04 -0600

    Passing customer index render test

--- spec/requests/api/v1/customers_spec.rb
+++ spec/requests/api/v1/customers_spec.rb
@@ -9,10 +9,11 @@ describe "Customers API" do
     expect(response).to be_successful
     desired_attributes = ['id','first_name','last_name']
     items = JSON.parse(response.body)
-    assert(items['data'].count).to eq(3)
+    expect(items['data'].count).to eq(3)
     items['data'].each do |customer|
       desired_attributes.each do |attribute|
-        assert(customer['attributes'].include?(attribute)).to eq(true)
+        assert(customer['attributes'].include?(attribute))
+      end
     end
   end
 end
