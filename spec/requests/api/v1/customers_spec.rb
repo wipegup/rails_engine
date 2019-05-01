a35b734202b04c004777140208160f94f82fd2c9
wipegup <wipegup@gmail.com>
2019-05-01 15:03:47 -0600

    Test for customer invoices

--- spec/requests/api/v1/customers_spec.rb
+++ spec/requests/api/v1/customers_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 describe "Customers API" do
   before :each do
-    create_list(:customer, 3)
+    customers = create_list(:customer, 3)
     @desired_attributes = ['id','first_name','last_name']
+
+    customers.each do |customer|
+      create_list(:invoice, 3, customer:customer)
+    end
   end
 
   it "sends a list of customers" do
@@ -37,6 +41,22 @@ describe "Customers API" do
     expect(response).to be_successful
     item = JSON.parse(response.body)
     @desired_attributes.each do |attribute|
+      assert(item['data']['attributes'].include?(attribute))
+    end
+  end
+
+  it 'returns all customer invoices' do
+    customer = Customer.last
+    get "/api/v1/customers/#{customer.id}/items.json"
+
+    invoice_attributes = ['id','merchant_id','customer_id']
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body)
+
+    expect(items['data'].count).to eq(3)
+
+    invoice_attributes.each do |attribute|
       assert(item['data']['attributes'].include?(attribute))
     end
   end
