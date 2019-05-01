3de8766f4e2c87bae561bd0d7e4676300960d2a5
wipegup <wipegup@gmail.com>
2019-05-01 14:10:44 -0600

    Test for single merchant show

--- spec/requests/api/v1/customers_spec.rb
+++ spec/requests/api/v1/customers_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 describe "Customers API" do
-  it "sends a list of customers" do
+  before :each do
     create_list(:customer, 3)
+  end
+
+  it "sends a list of customers" do
 
     get '/api/v1/customers.json'
 
@@ -16,4 +19,17 @@ describe "Customers API" do
       end
     end
   end
+
+  it "sends a single customer of customers" do
+
+    get '/api/v1/customers/1.json'
+
+    expect(response).to be_successful
+    desired_attributes = ['id','first_name','last_name']
+    item = JSON.parse(response.body)
+    desired_attributes.each do |attribute|
+      assert(item['data']['attributes'].include?(attribute))
+    end
+  end
+
 end
