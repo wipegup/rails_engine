c1ce4947c59349a3020136ea04bde497bae8b4d0
wipegup <wipegup@gmail.com>
2019-05-01 12:57:32 -0600

    Add test for desired json data for customer

--- spec/requests/api/v1/customers_spec.rb
+++ spec/requests/api/v1/customers_spec.rb
@@ -7,8 +7,12 @@ describe "Customers API" do
     get '/api/v1/customers.json'
 
     expect(response).to be_successful
-
+    desired_attributes = ['id','first_name','last_name']
     items = JSON.parse(response.body)
-    
+    assert(items['data'].count).to eq(3)
+    items['data'].each do |customer|
+      desired_attributes.each do |attribute|
+        assert(customer['attributes'].include?(attribute)).to eq(true)
+    end
   end
 end
