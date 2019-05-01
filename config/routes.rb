6de27ecd4c9543bd1516ba29a9472774d0dc12f9
wipegup <wipegup@gmail.com>
2019-05-01 14:13:43 -0600

    Passing test for single customer info

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index]
-      resources :customers, only: [:index]
+      resources :customers, only: [:index, :show]
       resources :items, only: [:index]
       resources :invoices, only: [:index]
       resources :invoice_items, only: [:index]
--- spec/requests/api/v1/customers_spec.rb
+++ spec/requests/api/v1/customers_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe "Customers API" do
   before :each do
     create_list(:customer, 3)
+    @desired_attributes = ['id','first_name','last_name']
   end
 
   it "sends a list of customers" do
@@ -10,24 +11,22 @@ describe "Customers API" do
     get '/api/v1/customers.json'
 
     expect(response).to be_successful
-    desired_attributes = ['id','first_name','last_name']
     items = JSON.parse(response.body)
     expect(items['data'].count).to eq(3)
     items['data'].each do |customer|
-      desired_attributes.each do |attribute|
+      @desired_attributes.each do |attribute|
         assert(customer['attributes'].include?(attribute))
       end
     end
   end
 
-  it "sends a single customer of customers" do
-
-    get '/api/v1/customers/1.json'
+  it "sends a single customer" do
+    cust_id = Customer.last.id
+    get "/api/v1/customers/#{cust_id}.json"
 
     expect(response).to be_successful
-    desired_attributes = ['id','first_name','last_name']
     item = JSON.parse(response.body)
-    desired_attributes.each do |attribute|
+    @desired_attributes.each do |attribute|
       assert(item['data']['attributes'].include?(attribute))
     end
   end
