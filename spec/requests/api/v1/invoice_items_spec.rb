3a6ba7e727d7e545d35dcf86ec37f6d209ae4561
wipegup <wipegup@gmail.com>
2019-05-01 14:17:43 -0600

    Tests for invoice items

--- spec/requests/api/v1/invoice_items_spec.rb
+++ spec/requests/api/v1/invoice_items_spec.rb
@@ -1,11 +1,34 @@
 require 'rails_helper'
 
 describe "Invoice Items API" do
+  before :each do
+    create_list(:invoice_item, 3)
+    @desired_attributes = ['id','item_id','invoice_id','quantity','unit_price']
+  end
   it "sends a list of invoice_items" do
     create_list(:invoice_item, 3)
 
     get '/api/v1/invoice_items.json'
 
     expect(response).to be_successful
+    items = JSON.parse(response.body)
+    expect(items['data'].count).to eq(3)
+    items['data'].each do |invoice_item|
+      @desired_attributes.each do |attribute|
+        assert(invoice_item['attributes'].include?(attribute))
+      end
+    end
   end
+
+  it "sends a single invoice_item" do
+    id = InvoiceItem.last.id
+    get "/api/v1/invoice_item/#{id}.json"
+
+    expect(response).to be_successful
+    item = JSON.parse(response.body)
+    @desired_attributes.each do |attribute|
+      assert(item['data']['attributes'].include?(attribute))
+    end
+  end
+
 end
